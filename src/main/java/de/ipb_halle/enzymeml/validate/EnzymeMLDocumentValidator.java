24f2d461d326acc488a9c287657ded45024a924a
Fabian Mauz <fabian66@freenet.de>
2026-02-12 15:45:16 +0100

    feat: add check for parameter and measurement uniques

--- src/main/java/de/ipb_halle/enzymeml/validate/EnzymeMLDocumentValidator.java
+++ src/main/java/de/ipb_halle/enzymeml/validate/EnzymeMLDocumentValidator.java
@@ -1,6 +1,8 @@
 package de.ipb_halle.enzymeml.validate;
 
 import de.ipb_halle.enzymeml.model.EnzymeMLDocument;
+import de.ipb_halle.enzymeml.model.Measurement;
+import de.ipb_halle.enzymeml.model.Parameter;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -11,15 +13,21 @@ import java.util.Set;
  * @author Fabian Mauz
  */
 public class EnzymeMLDocumentValidator {
-    
-    private Set<String> unitIds = new HashSet<>();
-    private Set<String> speciesId = new HashSet<>();
-    private EnzymeMLDocument document;
-    
-    public void validate(EnzymeMLDocument document) throws ValidationException {
+
+    private final Set<String> speciesId = new HashSet<>();
+    private final Set<String> measurementIds = new HashSet<>();
+    private final Set<String> parameterIds = new HashSet<>();
+    private final EnzymeMLDocument document;
+
+    public EnzymeMLDocumentValidator(EnzymeMLDocument document) {
         this.document = document;
     }
-    
+
+    public void validate() throws ValidationException {
+        checkForUniqueIds();
+        checkForExistingIdsOfReactionElements();
+    }
+
     private void checkForUniqueIds() throws ValidationException {
         for (String id : collectIdsOfPossibleSpecies()) {
             if (speciesId.contains(id)) {
@@ -27,21 +35,34 @@ public class EnzymeMLDocumentValidator {
             } else {
                 speciesId.add(id);
             }
-            
+        }
+        for (Measurement measurement : document.getMeasurements()) {
+            if (measurementIds.contains(measurement.getId())) {
+                throw new ValidationException("ID: " + measurement.getId() + " already used");
+            } else {
+                measurementIds.add(measurement.getId());
+            }
+        }
+        for (Parameter parameter : document.getParameters()) {
+            if (parameterIds.contains(parameter.getId())) {
+                throw new ValidationException("ID: " + parameter.getId() + " already used");
+            } else {
+                parameterIds.add(parameter.getId());
+            }
         }
     }
-    
+
     private void checkForExistingIdsOfReactionElements() throws ValidationException {
-        
+
     }
-    
+
     private List<String> collectIdsOfPossibleSpecies() {
         List<String> ids = new ArrayList<>();
         ids.addAll(document.getComplexes().stream().map(c -> c.getId()).toList());
         ids.addAll(document.getProteins().stream().map(c -> c.getId()).toList());
         ids.addAll(document.getSmallMolecules().stream().map(c -> c.getId()).toList());
-        
+
         return ids;
     }
-    
+
 }
