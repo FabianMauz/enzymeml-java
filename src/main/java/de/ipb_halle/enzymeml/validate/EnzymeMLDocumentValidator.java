12123fb6adbde99accf9a45b838f0fce07c0121e
Fabian Mauz <fabian66@freenet.de>
2026-02-12 15:38:42 +0100

    feat: add unique check for species id

--- src/main/java/de/ipb_halle/enzymeml/validate/EnzymeMLDocumentValidator.java
+++ src/main/java/de/ipb_halle/enzymeml/validate/EnzymeMLDocumentValidator.java
@@ -1,7 +1,9 @@
 package de.ipb_halle.enzymeml.validate;
 
 import de.ipb_halle.enzymeml.model.EnzymeMLDocument;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -9,23 +11,37 @@ import java.util.Set;
  * @author Fabian Mauz
  */
 public class EnzymeMLDocumentValidator {
-
+    
     private Set<String> unitIds = new HashSet<>();
     private Set<String> speciesId = new HashSet<>();
-
+    private EnzymeMLDocument document;
+    
     public void validate(EnzymeMLDocument document) throws ValidationException {
-
+        this.document = document;
     }
-
+    
     private void checkForUniqueIds() throws ValidationException {
-
+        for (String id : collectIdsOfPossibleSpecies()) {
+            if (speciesId.contains(id)) {
+                throw new ValidationException("ID: " + id + " already used");
+            } else {
+                speciesId.add(id);
+            }
+            
+        }
     }
-
+    
     private void checkForExistingIdsOfReactionElements() throws ValidationException {
-
+        
     }
-
-    private void collectIds() {
-
+    
+    private List<String> collectIdsOfPossibleSpecies() {
+        List<String> ids = new ArrayList<>();
+        ids.addAll(document.getComplexes().stream().map(c -> c.getId()).toList());
+        ids.addAll(document.getProteins().stream().map(c -> c.getId()).toList());
+        ids.addAll(document.getSmallMolecules().stream().map(c -> c.getId()).toList());
+        
+        return ids;
     }
+    
 }
