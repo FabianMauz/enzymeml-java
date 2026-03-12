f7bf61efc13473094e4ba56f0563b94955dbe03b
FabianMauz <fmauz@ipb-halle.de>
2026-03-12 09:12:12 +0100

    fix: add custom serializer for Reaction xml

---
+++ src/main/java/de/ipb_halle/enzymeml/serialize/ReactionXmlSerializer.java
@@ -0,0 +1,75 @@
+package de.ipb_halle.enzymeml.serialize;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator;
+import de.ipb_halle.enzymeml.model.Equation;
+import de.ipb_halle.enzymeml.model.ModifierElement;
+import de.ipb_halle.enzymeml.model.Reaction;
+import de.ipb_halle.enzymeml.model.ReactionElement;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author Fabian Mauz (fmauz@ipb-halle.de)
+ */
+public class ReactionXmlSerializer extends StdSerializer<Reaction> {
+
+    public ReactionXmlSerializer() {
+        super(Reaction.class);
+    }
+
+    @Override
+    public void serialize(Reaction reaction, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        ToXmlGenerator xmlGen = (ToXmlGenerator) gen;
+
+        xmlGen.writeStartObject();
+        xmlGen.writeStringField("id", reaction.getId());
+        xmlGen.writeStringField("name", reaction.getName());
+        xmlGen.writeBooleanField("reversible", reaction.isReversible());
+
+        Equation kineticLaw = reaction.getKineticLaw();
+        if (kineticLaw != null) {
+            xmlGen.writeFieldName("kinetic_law");
+            xmlGen.writeStartObject();
+            xmlGen.writeObjectField("Equation", kineticLaw);
+            xmlGen.writeEndObject();
+        }
+
+        writeReactionElements(xmlGen, "reactants", reaction.getReactants());
+        writeReactionElements(xmlGen, "products", reaction.getProducts());
+        writeModifierElements(xmlGen, "modifiers", reaction.getModifiers());
+
+        xmlGen.writeEndObject();
+    }
+
+    private void writeReactionElements(ToXmlGenerator xmlGen, String wrapperName,
+            List<ReactionElement> elements) throws IOException {
+        xmlGen.writeFieldName(wrapperName);
+        xmlGen.writeStartObject();
+        if (elements != null) {
+            for (ReactionElement element : elements) {
+                if (element != null) {
+                    xmlGen.writeObjectField("ReactionElement", element);
+                }
+            }
+        }
+        xmlGen.writeEndObject();
+    }
+
+    private void writeModifierElements(ToXmlGenerator xmlGen, String wrapperName,
+            List<ModifierElement> elements) throws IOException {
+        xmlGen.writeFieldName(wrapperName);
+        xmlGen.writeStartObject();
+        if (elements != null) {
+            for (ModifierElement element : elements) {
+                if (element != null) {
+                    xmlGen.writeObjectField("ModifierElement", element);
+                }
+            }
+        }
+        xmlGen.writeEndObject();
+    }
+}
--- src/main/java/de/ipb_halle/enzymeml/serialize/mixins/xml/ReactionXmlMixin.java
+++ src/main/java/de/ipb_halle/enzymeml/serialize/mixins/xml/ReactionXmlMixin.java
@@ -1,30 +1,12 @@
 package de.ipb_halle.enzymeml.serialize.mixins.xml;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import de.ipb_halle.enzymeml.model.ModifierElement;
-import de.ipb_halle.enzymeml.model.ReactionElement;
-import java.util.List;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import de.ipb_halle.enzymeml.serialize.ReactionXmlSerializer;
 
 /**
  *
  * @author Fabian Mauz (fmauz@ipb-halle.de)
  */
+@JsonSerialize(using = ReactionXmlSerializer.class)
 public interface ReactionXmlMixin {
-
-    @JacksonXmlProperty(localName = "ReactionElement")
-    @JsonProperty("reactantsList") // Unique ID 1
-    @JacksonXmlElementWrapper(localName = "reactants") // Outer tag  
-    List<ReactionElement> getReactants();
-
-    @JacksonXmlElementWrapper(localName = "products") // Outer tag
-    @JsonProperty("productsList") // Unique ID 1
-    @JacksonXmlProperty(localName = "ReactionElement")
-    List<ReactionElement> getProducts();
-
-    @JsonProperty("modifiers") // Logical name for Jackson
-    @JacksonXmlElementWrapper(localName = "modifiers") // Outer tag
-    @JacksonXmlProperty(localName = "ModifierElement") // Inner tag
-    List<ModifierElement> getModifiers();
 }
