85b490b8bce47162696892621f843da5160e5085
Fabian Mauz <fmauz@ipb-halle.de>
2026-03-19 09:06:27 +0100

    test: add test for deserialisation of rich document

--- src/test/java/de/ipb_halle/enzymeml/serialize/JsonDeserializerTest.java
+++ src/test/java/de/ipb_halle/enzymeml/serialize/JsonDeserializerTest.java
@@ -41,6 +41,19 @@ public class JsonDeserializerTest {
     }
 
     @Test
+    public void deserialize_ofDocument_returnDocument() throws ValidationException, IOException {
+        EnzymeMLDocument document = deserializer.deserialize(new File("src/test/resources/fixtures/json/withOneDocument.json"));
+        Assertions.assertEquals("Example Document", document.getName());
+        Assertions.assertEquals("2.0", document.getVersion());
+        Assertions.assertEquals("2025-01-01", document.getCreated());
+        Assertions.assertEquals("2025-01-01", document.getModified());
+        Assertions.assertEquals("Description of document", document.getDescription());
+        Assertions.assertEquals(2, document.getReferences().size());
+        Assertions.assertTrue(document.getReferences().contains("ref-1"));
+        Assertions.assertTrue(document.getReferences().contains("ref-2"));
+    }
+
+    @Test
     public void deserialize_ofMinimalEquation_returnDocumentWithEquation() throws ValidationException, IOException {
         EnzymeMLDocument document = deserializer.deserialize(new File("src/test/resources/fixtures/json/withMinimalEquation.json"));
         Assertions.assertEquals("Example Document", document.getName());
