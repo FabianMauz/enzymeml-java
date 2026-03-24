6542077c2c744bd0310123b351e39ea41b4e3c91
Fabian Mauz <fmauz@ipb-halle.de>
2026-03-24 14:13:54 +0100

    fix: add constructor for parameter xml mixin

--- src/main/java/de/ipb_halle/enzymeml/serialize/mixins/xml/ParameterXmlMixin.java
+++ src/main/java/de/ipb_halle/enzymeml/serialize/mixins/xml/ParameterXmlMixin.java
@@ -1,46 +1,56 @@
 package de.ipb_halle.enzymeml.serialize.mixins.xml;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import de.ipb_halle.enzymeml.model.UnitDefinition;
+import de.ipb_halle.enzymeml.validate.ValidationException;
 
 /**
  *
  * @author Fabian Mauz (fmauz@ipb-halle.de)
  */
 @JsonPropertyOrder({"id", "name", "symbol", "value", "unit", "initial_value", "upper_bound", "lower_bound", "fit", "stderr", "constant"})
-public interface ParameterXmlMixin {
+public abstract class ParameterXmlMixin {
+
+    @JsonCreator
+    public ParameterXmlMixin(
+            @JsonProperty("id") String id,
+            @JsonProperty("name") String name,
+            @JsonProperty("symbol") String symbol) throws ValidationException {
+    }
 
     @JacksonXmlProperty(localName = "id")
-    String getId();
+    abstract String getId();
 
     @JacksonXmlProperty(localName = "name")
-    String getName();
+    abstract String getName();
 
     @JacksonXmlProperty(localName = "symbol")
-    String getSymbol();
+    abstract String getSymbol();
 
     @JacksonXmlProperty(localName = "value")
-    Float getValue();
+    abstract Float getValue();
 
     @JacksonXmlProperty(localName = "unit")
-    UnitDefinition getUnit();
+    abstract UnitDefinition getUnit();
 
     @JacksonXmlProperty(localName = "initial_value")
-    Float getInitialValue();
+    abstract Float getInitialValue();
 
     @JacksonXmlProperty(localName = "upper_bound")
-    Float getUpperBound();
+    abstract Float getUpperBound();
 
     @JacksonXmlProperty(localName = "lower_bound")
-    Float getLowerBound();
+    abstract Float getLowerBound();
 
     @JacksonXmlProperty(localName = "fit")
-    Boolean getFit();
+    abstract Boolean getFit();
 
     @JacksonXmlProperty(localName = "stderr")
-    Float getStderr();
+    abstract Float getStderr();
 
     @JacksonXmlProperty(localName = "constant")
-    Boolean isConstant();
+    abstract Boolean isConstant();
 }
